1ea66f793cec66df6afb672bab521ac6c20a7779
Robins-2603 <robinsforwork@gmail.com>
2022-08-04 19:47:19 +0530

    Refactored: whole code acc yo function use

--- EmpWage.js
+++ EmpWage.js
@@ -1,46 +1,36 @@
 console.log("Welcome to the employee wage programs.");
 /*
-UC1-checking emp status
-Ability to check Employee is present or absent by using Random Function
+UC3- Refactor the Code
+to write a function
+to get work hours
 */
-// updated the code according to UC2
 const IS_ABSENT = 0;
 const IS_PartTime = 1;
 const IS_FullTime = 2;
 const PartTimeHrs = 4;
 const FullTimeHrs = 8;
 const EmpWagePerHr = 20;
-    let employeeCheck = Math.floor(Math.random() * 10 ) % 3; 
-   // console.log(employeeCheck);
-
-    // if(employeeCheck == IS_ABSENT){
-    //     console.log("Employee is ABSENT.");
-    // }if(employeeCheck == IS_FullTime){
-    //     console.log("Employee is PRESENT.");
-    // }else{
-    //     console.log("Employee is HalfDay Working.")
-    // }
-
-
-//UC_2 Switch case
-
-
-let empHrs = 0;
-// let empCheck = Math.floor(Math.random()*10)%3;
-switch(employeeCheck) {
-    case IS_PartTime:
-        empHrs = PartTimeHrs; 
-        console.log("Employee is HalfDay Working.") 
-        break;
-    case IS_FullTime:
-        empHrs = FullTimeHrs;
-        console.log("Employee is PRESENT.");
-        break;
-    default:
-        empHrs = 0; 
-        console.log("Employee is ABSENT.");       
+    
+// using function
+function getWorkingHrs(employeeCheck) {
+    switch(employeeCheck) {
+        case IS_PartTime:
+            return PartTimeHrs; 
+            console.log("Employee is HalfDay Working.") 
+        
+        case IS_FullTime:
+            return FullTimeHrs;
+            console.log("Employee is PRESENT.");
+            break;
+        default:
+            return 0; 
+            console.log("Employee is ABSENT.");       
+    }
 }
+let employeeCheck = Math.floor(Math.random() * 10 ) % 3;
+let empHrs = getWorkingHrs(employeeCheck);
 
-// calculating the empwage
+// calculating the empwage and empHrs
 let empWage = empHrs*EmpWagePerHr;
 console.log("EmpWage: " +empWage);
+console.log("EmpHrs: " +empHrs);
