e3434cb47cc82aaa3d55ff9d1efd0fb040819959
Robins-2603 <robinsforwork@gmail.com>
2022-08-04 17:37:19 +0530

    added: UC1 for empWage

---
+++ EmpWage.js
@@ -0,0 +1,13 @@
+console.log("Welcome to the employee wage programs.");
+/*
+Ability to check Employee is present or absent by using Random Function
+*/
+const IS_ABSENT = 0;
+let employeeCheck = Math.floor(Math.random() * 10 ) % 2; 
+console.log(employeeCheck);
+
+if(employeeCheck == IS_ABSENT){
+    console.log("Employee is ABSENT.");
+}else{
+    console.log("Employee is PRESENT.");
+}
