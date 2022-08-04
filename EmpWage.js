7a2980540d667dd220711c2953e85002eb2e8bc4
Robins-2603 <robinsforwork@gmail.com>
2022-08-05 01:22:58 +0530

    modified: code with while loop for fullfilling given condition

--- EmpWage.js
+++ EmpWage.js
@@ -1,7 +1,8 @@
 console.log("Welcome to the employee wage programs.");
 /*
-UC4- Calculating Wages for a Month 
-assuming 20 Working Days in a month
+UC4- Calculate Wages till a condition of total
+working hours of 160 or max days of 20 is
+reached for a month
 */
 const IS_ABSENT = 0;
 const IS_PartTime = 1;
@@ -9,7 +10,8 @@ const IS_FullTime = 2;
 const PartTimeHrs = 4;
 const FullTimeHrs = 8;
 const EmpWagePerHr = 20;
-const totalWorkingDays = 20;
+const maxWorkingDays = 20;
+const maxWorkingHrs = 160;
     
 // using function
 function getWorkingHrs(employeeCheck) {
@@ -28,18 +30,21 @@ function getWorkingHrs(employeeCheck) {
     }
 }
    /* 
-   introducing the for loop for calculating Empwage
+   introducing the while loop by replacing for loop for calculating Empwage
    here we put employeecheck in the loop
-   as well as adding the hours to get totalEmpHrs
+   as well as adding the hours to get totalEmpHrs and totalWorkingDays also
    */
 
 let totalEmpHrs = 0;
-for(let day = 0; day<totalWorkingDays; day++){
+let totalWorkingDays = 0;
+while(totalEmpHrs <= maxWorkingHrs && totalWorkingDays <= maxWorkingDays){ 
+    totalWorkingDays++;
     let employeeCheck = Math.floor(Math.random() * 10 ) % 3;
     totalEmpHrs += getWorkingHrs(employeeCheck); 
 }
 
-// calculating the empwage and empHrs
+// calculating the empwage and empHrs and here we printing totalWorkingDays as well
 let empWage = totalEmpHrs*EmpWagePerHr;
 console.log("EmpWage: " +empWage);
 console.log("Total EmpHrs: " +totalEmpHrs);
+console.log("Total Working Days: " +totalWorkingDays);
