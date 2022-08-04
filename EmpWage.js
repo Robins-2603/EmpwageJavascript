99ff374d956b53288ae3e2722a4374b82cc95081
Robins-2603 <robinsforwork@gmail.com>
2022-08-05 00:58:42 +0530

    modified: code by using for loop to get whole month wage

--- EmpWage.js
+++ EmpWage.js
@@ -1,8 +1,7 @@
 console.log("Welcome to the employee wage programs.");
 /*
-UC3- Refactor the Code
-to write a function
-to get work hours
+UC4- Calculating Wages for a Month 
+assuming 20 Working Days in a month
 */
 const IS_ABSENT = 0;
 const IS_PartTime = 1;
@@ -10,6 +9,7 @@ const IS_FullTime = 2;
 const PartTimeHrs = 4;
 const FullTimeHrs = 8;
 const EmpWagePerHr = 20;
+const totalWorkingDays = 20;
     
 // using function
 function getWorkingHrs(employeeCheck) {
@@ -27,10 +27,15 @@ function getWorkingHrs(employeeCheck) {
             console.log("Employee is ABSENT.");       
     }
 }
-let employeeCheck = Math.floor(Math.random() * 10 ) % 3;
-let empHrs = getWorkingHrs(employeeCheck);
+   
+
+let totalEmpHrs = 0;
+for(let day = 0; day<totalWorkingDays; day++){
+    let employeeCheck = Math.floor(Math.random() * 10 ) % 3;
+    totalEmpHrs += getWorkingHrs(employeeCheck); 
+}
 
 // calculating the empwage and empHrs
-let empWage = empHrs*EmpWagePerHr;
+let empWage = totalEmpHrs*EmpWagePerHr;
 console.log("EmpWage: " +empWage);
-console.log("EmpHrs: " +empHrs);
+console.log("Total EmpHrs: " +totalEmpHrs);
